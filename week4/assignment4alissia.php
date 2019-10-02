ce16fcce3bad403bd3f03b8445155f77464f15d7
ahoorn373 <55299420+ahoorn373@users.noreply.github.com>
2019-10-02 14:48:30 -0700

    assignment 4 answers submitted by Alissia Hoorn
    answers to question 1 to 4 in file attached

---
+++ week4/assignment4alissia.php
@@ -0,0 +1,44 @@
+Answer to Q1.  
+- PHP is used for backend programming like after submitting a form where the data should go, so used for every backend logic design.
+ - Javascript is used for making website responsive and interactive for front-end design.
+
+
+Answer Q2.  
+When Using GET the variables are transmitted in the page URL which can be seen by everyone but POST does not so with sensitive info like passwords you use POST. 
+Get Information is what you see attatched to the end of some urls. it can be accesed with example: $_GET['var2'].
+Post fields are considered more secure, it can be accessed by the get field with $_POST.
+
+
+Answer Q3.
+- For loop is used where we already know about the number of times loop needs to be excuted. Typically for a index used in iteration.
+- While loop is used in situations where we do not know how many times loop needs to be excuted beforehand.
+
+Answer Q4.
+
+example of php String:  
+
+<?php 
+echo str_word_count("Hello World!");
+?>
+
+example of php Integer:
+
+<?php
+$x = 5985;
+var_dump($x);
+?>
+
+example of php Double:
+
+<?php
+$a = 1.234;
+$b = 1.2e3;
+$c = 7E-10;
+?>
+
+
+example of php Boolean:
+Booleans represents two possible states: True or False
+$x = true;
+$y = false;
+
